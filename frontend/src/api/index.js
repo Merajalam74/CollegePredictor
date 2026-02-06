ae973a1c33877eff31af61b82deff3ee4e964bfb
MD MERAJ ALAM <merajalamnazeeri@gmail.com>
2026-02-06 22:23:42 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ||'http://localhost:4000/api', // Ensure this matches your backend port
+  baseURL: import.meta.env.VITE_API_URL ||'http://localhost:4000/api', 
 });
 
 api.interceptors.request.use((config) => {
@@ -15,7 +15,6 @@ api.interceptors.request.use((config) => {
     }
   } catch (e) {
     console.error("Error parsing userInfo from localStorage", e);
-    // Optionally clear corrupted data: localStorage.removeItem('userInfo');
   }
   return config;
 });
