e59ddbd6c77bca54a093e6c97f1bacd4e4afbcf9
MD MERAJ ALAM <merajalamnazeeri@gmail.com>
2026-02-06 15:34:59 +0530

    Linked to backend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api', // Ensure this matches your backend port
+  baseURL: import.meta.env.VITE_API_URL ||'http://localhost:4000/api', // Ensure this matches your backend port
 });
 
 api.interceptors.request.use((config) => {
