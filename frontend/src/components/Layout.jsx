39f3401c2e80b310f6d7daa8b0ee15a55eb7b1cf
MD MERAJ ALAM <merajalamnazeeri@gmail.com>
2026-02-06 22:26:51 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer.jsx';
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-950"> {/* Adjusted background */}
+    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-950"> 
       <Header />
       <main className="flex-1 container mx-auto p-4 lg:p-10">
         {children}
